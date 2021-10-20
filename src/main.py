ca27c2a29bbc9747d80a5235004c0846aa65495b
thomasport <thomasportugal5@gmail.com>
2021-10-19 22:08:03 -0300

    add load data

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ def main():
     return
 
 def load_data(path_data="../data/"):
-    return
+    ori_data = scipy.io.loadmat(path_data+"ori.mat")
+    ori_noise_10 = scipy.io.loadmat(path_data+"ori_noise_10.mat")
+    return ori_data, ori_noise_10 
 
 if __name__ == "__main__" :
     main()
